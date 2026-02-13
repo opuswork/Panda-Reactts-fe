1a2aa7959a1ccbd2e5c6fc6277e08e3b6c25e8c1
Taeyoung Seon <tseon@opuscore.net>
2026-02-13 12:49:18 +0900

    feat(landing) add landing page, Header, Footer-missing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 
 import './App.css'
 import { Landing } from './features/landing/Landing'
+import { Header } from './components/layout/Header'
+import { Footer } from './components/layout/Footer'
 
 function App() {
   return (
     <>
+      <Header />
       <Landing />
+      <Footer />
     </>
   )
 }
